c34b80601ac390db1bcab7d1e3122770ed8dbd06
TruongProVc <gosumodeon@gmail.com>
2024-09-08 16:59:17 +0700

    9/8

---
+++ WebApplication1/WebApplication1/Controllers/AllItemController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using PagedList;
+namespace WebApplication1.Controllers
+{
+    public class AllItemController : Controller
+    {
+        static ShoesShopEntities db = new ShoesShopEntities();
+
+        public ActionResult Index(int? page, int? maThuongHieu)
+        {
+            var pageNumber = page ?? 1;
+            var pageSize = 9;
+
+            var products = db.SanPhams.AsQueryable();
+
+            if (maThuongHieu.HasValue)
+            {
+                products = products.Where(p => p.maThuongHieu == maThuongHieu.Value);
+                ViewBag.maThuongHieu = maThuongHieu.Value;
+            }
+
+            var model = products.OrderBy(p => p.tenSanPham).ToPagedList(pageNumber, pageSize);
+
+            ViewBag.ThuongHieus = db.ThuongHieux.ToList();
+            ViewBag.Sizes = db.SizeGiays.ToList();
+
+            return View(model);
+        }
+
+        //load lại sản phẩm khi dùng ajax
+        [HttpGet]
+        public ActionResult LoadProducts(int? page, int? maThuongHieu)
+        {
+            var pageNumber = page ?? 1;
+            var pageSize = 9;
+
+            var products = db.SanPhams.AsQueryable();
+
+            if (maThuongHieu.HasValue)
+            {
+                products = products.Where(p => p.maThuongHieu == maThuongHieu.Value);
+            }
+
+            var sp = products.OrderBy(p => p.tenSanPham).ToPagedList(pageNumber, pageSize);
+
+            if (!sp.Any())
+            {
+                return PartialView("_NoProductsPartial");
+            }
+            else
+            {
+                return PartialView("_ProductListPartial", sp);
+            }
+        }
+        // Lọc giá tiền 
+        public ActionResult LoadProductsByPriceRange(int minPrice, int maxPrice, int? page)
+        {
+            var pageNumber = page ?? 1;
+            var pageSize = 9;
+
+            var filteredProducts = db.SanPhams
+                .Where(sp => sp.giaTien >= minPrice && sp.giaTien <= maxPrice)
+                .OrderBy(sp => sp.tenSanPham)
+                .ToPagedList(pageNumber, pageSize);
+
+            return PartialView("_ProductListPartial", filteredProducts);
+        }
+        //Lọc theo size
+        public ActionResult FilterBySize(int size, int? page)
+        {
+            var pageNumber = page ?? 1;
+            var pageSize = 9;
+
+            var products = from spSize in db.SanPhamSizes
+                           join sizeGiay in db.SizeGiays on spSize.maSize equals sizeGiay.maSize
+                           where sizeGiay.size == size
+                           select spSize.maSanPham;
+
+            var filteredProducts = db.SanPhams
+                                     .Where(p => products.Contains(p.maSanPham))
+                                     .OrderBy(p => p.tenSanPham)
+                                     .ToPagedList(pageNumber, pageSize);
+            if (!filteredProducts.Any() && !products.Any())
+            {
+                return PartialView("_NoProductsPartial");
+            }
+
+            return PartialView("_ProductListPartial", filteredProducts);
+        }
+
+    }
+
+}
